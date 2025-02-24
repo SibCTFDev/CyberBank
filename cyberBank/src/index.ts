d398c39c464eb5f63be3e99eeeb9b46a086dfe72
Romanashenko Ivan <i.romanashenko@cft.ru>
2025-02-24 15:44:04 +0700

    init app structure

---
+++ cyberBank/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express"
+
+
+export const app = express();
+
+
+const port = process.env.PORT || 8000;
+app.get('/', (request, response) => {
+  response.send('Hello world!');
+});
+app.listen(port, () => console.log(`Running on port ${port}`));
