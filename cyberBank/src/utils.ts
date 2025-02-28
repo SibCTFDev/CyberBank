a80be045b531cc369d5d4598d3c09fc8cbe805eb
Stix-tomsk <iromanashenko@mail.ru>
2025-02-28 19:37:26 +0700

    add example endpoint routing

--- cyberBank/src/utils.ts
+++ cyberBank/src/utils.ts
@@ -1,10 +1,11 @@
 import { AuthController, LogoutController } from './controller/authControllers';
 import { ProductsController, CreateProductController } from './controller/productsControllers';
+import { ExampleController } from './controller/exampleController';
 
 
 export function getControllersList() : Function[] {
     return [
         AuthController, LogoutController, 
-        ProductsController, CreateProductController
+        ProductsController, CreateProductController, ExampleController
     ];
 }
